a153af38a167c77d628e9d532f23d3f69c814497
Hugo Vouillamoz <hugo.vouillamoz@students.hevs.ch>
2022-03-14 21:41:22 +0100

    get all rides on the main page

--- app/src/main/java/com/example/Horse_App/activities/MainPage.java
+++ app/src/main/java/com/example/Horse_App/activities/MainPage.java
@@ -1,16 +1,61 @@
 package com.example.Horse_App.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.Horse_App.BaseApp;
+import com.example.Horse_App.CustomCourseItem;
+import com.example.Horse_App.Database.Entity.Ride;
+import com.example.Horse_App.Database.repository.RideRepository;
 import com.example.Horse_App.R;
+import com.example.Horse_App.ViewModel.RideListViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
+
+    private RideRepository repository;
+    private List<Ride> rides = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        startMainPage();
+    }
+
+    private void startMainPage(){
+
+        repository = ((BaseApp) getApplication()).getRideRepository();
+//        RideListViewModel.Factory factory = new RideListViewModel.Factory(getApplication());
+//        RideListViewModel viewModel = new ViewModelProvider(this, factory).get(RideListViewModel.class);
+
+//        RideListViewModel rideListViewModel = new RideListViewModel(getApplication(),repository);
+//        rideListViewModel.getRides().observe(this, ridesEntities -> {
+//            if(ridesEntities != null){
+//                System.out.println(ridesEntities.size());
+//                for(Ride ride : ridesEntities){
+//                    rides.add(ride);
+//                    System.out.println("SALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLUT");
+//                }
+//            }
+//        });
+
+        ListView listView = findViewById(R.id.ListRideToChoose);
+        repository.getRides(getApplication()).observe(this, ridesEntities -> {
+            if(ridesEntities != null){
+
+                    CustomCourseItem customCourseItem = new CustomCourseItem(this, ridesEntities);
+                    listView.setAdapter(customCourseItem);
+
+
+            }
+        });
+
     }
 }
