66ebeb44ba354f87f59e3e8abfbd3b6c17355d13
benjamin <benjamin.morel@students.hevs.ch>
2022-03-22 12:17:08 +0100

    Implementation of an animation splash screen

---
+++ app/src/main/java/com/example/Horse_App/activities/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.Horse_App.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.Horse_App.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView image;
+    TextView title, description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.splash_image);
+        title = findViewById(R.id.splash_title);
+        description = findViewById(R.id.splash_description);
+
+        image.setAnimation(topAnimation);
+        image.setAnimation(bottomAnimation);
+//        title.setText((CharSequence) bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                redirect();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+
+    private void redirect() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+}
